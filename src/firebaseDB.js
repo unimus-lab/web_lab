c3db4f8c3f9eb3c795a094be1eb8f7a7bb3645ad
sejodoh <teamsejodoh@gmail.com>
2022-05-21 12:11:59 +0700

    fixing some improvements

--- src/firebaseDB.js
+++ src/firebaseDB.js
@@ -1,13 +1,22 @@
 import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyCT4QIgdp-qEyKeImIKvpgacUgPzocNI-E",
-    authDomain: "unimuslab.firebaseapp.com",
-    projectId: "unimuslab",
-    storageBucket: "unimuslab.appspot.com",
-    messagingSenderId: "151603669601",
-    appId: "1:151603669601:web:085f3464fbd9744787d820"
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCT4QIgdp-qEyKeImIKvpgacUgPzocNI-E",
+//     authDomain: "unimuslab.firebaseapp.com",
+//     projectId: "unimuslab",
+//     storageBucket: "unimuslab.appspot.com",
+//     messagingSenderId: "151603669601",
+//     appId: "1:151603669601:web:085f3464fbd9744787d820"
+//   };
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDbIQDQWRWLAfRJMg5Vcx-VqA4zJaf2_yI",
+    authDomain: "unimus-lab-kimia.firebaseapp.com",
+    projectId: "unimus-lab-kimia",
+    storageBucket: "unimus-lab-kimia.appspot.com",
+    messagingSenderId: "511258221348",
+    appId: "1:511258221348:web:10b0a8fa8ec2068ce071b1"
   };
 
 // Initialize Firebase
